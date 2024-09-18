b9bdc8953816e02ce6df9675ec72667865987503
Juan Sandoval <juanchosando@outlook.es>
2024-09-17 20:48:13 -0600

    Ejercicio 7

---
+++ Ejercicio7/src/main/java/com/mycompany/ejercicio7/Ejercicio7.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio7;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        System.out.println("Números impares del 2 al 100:");
+        for (int i = 3; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
