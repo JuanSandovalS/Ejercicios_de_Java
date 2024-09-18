3b07ec87dddd0f7cd4b958c19b2301d63499108b
Juan Sandoval <juanchosando@outlook.es>
2024-09-17 20:43:40 -0600

    Ejercicio 2

---
+++ ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Introduce tu nombre: ");
+        String nombre = scanner.nextLine();
+        
+        System.out.println("Buenos dias, " + nombre + ".");
+    }
+}
