9425becba5c5e7caaa3800f519db8091a0b321cd
Juan Sandoval <juanchosando@outlook.es>
2024-09-17 20:47:38 -0600

    Ejercicio 6

---
+++ Ejercicio6/src/main/java/com/mycompany/ejercicio6/Ejercicio6.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio6;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+       System.out.println("Números pares del 2 al 100:");
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
